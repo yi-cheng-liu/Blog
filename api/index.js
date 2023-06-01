d90de3a28bf05eb9dc516106ba3922586a62c141
yi-cheng-liu <liuyiche@umich.edu>
2023-06-01 14:13:46 -0400

    blog with api

---
+++ api/index.js
@@ -0,0 +1,120 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+const User = require("./models/User");
+const Post = require("./models/Post");
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const uploadMiddleware = multer({ dest: 'uploads/' })
+
+const fs = require('fs');
+
+const salt = bcrypt.genSaltSync(10);
+const secret = "mysecretssdfgdsfgdsghhh";
+
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(express.json());
+app.use(cookieParser());
+
+mongoose
+  .connect(
+    "mongodb+srv://liuyiche:mGgOLYs4zDC2BQMh@cluster0.lr5dipb.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
+
+app.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.json(userDoc);
+  } catch (error) {
+    console.error("Error creating user:", error);
+    console.log(error);
+    res.status(500).json({ error: "Error creating user" });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const userDoc = await User.findOne({ username });
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+
+    if (!userDoc) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const passwordMatch = bcrypt.compareSync(password, userDoc.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Invalid password" });
+    }
+
+    // logged in
+    if (passOk) {
+      jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+        if (err) throw err;
+        res.cookie('token', token).json({
+          id: userDoc._id,
+          username,
+        });
+      });
+    }
+  }
+  catch (error) {
+    console.error("Error logging in:", error);
+    res.status(500).json({ error: "Error logging in" });
+  } 
+});
+
+app.get('/profile', (req, res) => { 
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+})
+
+app.post('/logout', (req, res) => {
+  res.cookie('token', '').json('logged out');
+})
+
+app.post('/post', uploadMiddleware.single('file'), async (req, res) => { 
+  if (req.file) {
+    const { originalname, path } = req.file;
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+    newPath = path + "." + ext;
+    fs.renameSync(path, newPath);
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, async(err, info) => {
+      if (err) throw err;
+      const { title, summary, content } = req.body;
+      const postDoc = await Post.create({
+        title, summary, content, cover: newPath, author:info.id,
+      })
+      res.json(postDoc);
+    });
+  }
+
+  
+})
+
+app.get('/post', async (req, res) => { 
+  res.json(await Post.find().populate('author', ['username']));
+})
+
+app.listen(4000, () => {
+  console.log("Server listening on port 4000");
+});
---
+++ api/models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const {Schema, model} = mongoose;
+const UserSchema = new Schema({
+    username: {type: String, required: true, unique: true, min: 4},
+    password: {type: String, required: true},
+});
+
+const UserModel = model('User', UserSchema);
+
+module.exports = UserModel;
